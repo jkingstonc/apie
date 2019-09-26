302994d168b480efe8a8440cdc1d07c2071892b9
james clarke <jkc200013@gmail.com>
2019-09-26 21:17:38 +0100

    added support for multiple connections

--- apie/net.py
+++ apie/net.py
@@ -19,33 +19,34 @@ class NetServer(Thread):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.address = (addr, port)
         self.socket.bind(self.address)
-
+    
     def run(self):
         self.socket.listen(1)
-        print("waiting for a connection")
-        connection, client_address = self.socket.accept()
+        while True:
+            print("waiting for a connection")
+            connection, client_address = self.socket.accept()
+            Thread(target=self.connection_handler, args=(connection, client_address)).start()
+
+    def connection_handler(self, connection, client_address):
         try:
             print("client connected: {}".format(client_address))
             if str(client_address[0]) in self.service.conn_list:
                 self.service.conn_list[client_address[0]]()
-            while True:
-                # In reality this is unnessescary as route strings are unlikely to be a kilobyte long
-                # however you never know...
-                request = b''.join(iter(functools.partial(connection.recv, BLOCK_SIZE), CLIENT_SENTINEL))
-                print("path request '{}'".format(request.decode('utf-8')))
-                if request:
-                    return_data = None
-                    if self.service.use_whitelist and client_address[0] not in self.service.whitelist:
-                        return_data = pickle.dumps("Your ip is not whitelisted!")
-                    elif client_address[0] in self.service.blacklist:
-                        return_data = pickle.dumps("Your ip is blacklisted!")
-                    else:
-                        return_data = pickle.dumps(self.service.visit_route(request.decode('utf-8')))
-                    for i in range(len(return_data) // BLOCK_SIZE + 1):
-                        connection.sendto(return_data[i * BLOCK_SIZE: (i + 1) * BLOCK_SIZE], client_address)
-                        connection.sendto(SERVER_SENTINEL, client_address)
+            # In reality this is unnessescary as route strings are unlikely to be a kilobyte long
+            # however you never know...
+            request = b''.join(iter(functools.partial(connection.recv, BLOCK_SIZE), CLIENT_SENTINEL))
+            print("path request '{}'".format(request.decode('utf-8')))
+            if request:
+                return_data = None
+                if self.service.use_whitelist and client_address[0] not in self.service.whitelist:
+                    return_data = pickle.dumps("Your ip is not whitelisted!")
+                elif client_address[0] in self.service.blacklist:
+                    return_data = pickle.dumps("Your ip is blacklisted!")
                 else:
-                    break
+                    return_data = pickle.dumps(self.service.visit_route(request.decode('utf-8')))
+                for i in range(len(return_data) // BLOCK_SIZE + 1):
+                    connection.sendto(return_data[i * BLOCK_SIZE: (i + 1) * BLOCK_SIZE], client_address)
+                    connection.sendto(SERVER_SENTINEL, client_address)
         finally:
             connection.close()
 
@@ -67,4 +68,5 @@ class NetClient(Thread):
             self.socket.sendto(str.encode(msg)[i * BLOCK_SIZE: (i + 1) * BLOCK_SIZE], addr)
             self.socket.sendto(CLIENT_SENTINEL, addr)
         data = pickle.loads(b''.join(iter(functools.partial(self.socket.recv, BLOCK_SIZE), SERVER_SENTINEL)))
+        self.socket.close()
         return data
