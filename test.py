6427563d9359f7f71385946ceea385aa1795ba0e
James Clarke <jkc200013@gmail.com>
2019-09-28 17:33:25 +0100

    changed how args work on routes

--- test.py
+++ test.py
@@ -6,23 +6,29 @@ from apie.net import NetClient
 from apie import protocol
 
 # Create a new APIE service
-s = Service(ip="localhost", port=3141)
+s = Service(ip="localhost")
 
 # Create an api route to return a float
 @s.route(path="multiply")
-def second_api(args):
-    return args[0] * args[1]
+def second_api(*args, **kwargs):
+    msg = "Your ip is '{}' and your result is {}".format(
+        kwargs.get("info")[0],
+        kwargs.get("args")[0] * kwargs.get("args")[1]
+    )
+    return msg
 
 if __name__ == "__main__":
     # Start the service
     s.start()
-    # Create a client for testing!
-    net = NetClient(ip="localhost", port=3141)
+    # Create a client for testing by connecting to service
+    net = NetClient(ip="localhost")
     # Multiply 2 numbers using the 'multiply' api we created
-    result = net.send("multiply", args=(3,4))
+    result = net.send("multiply", args=(3, 4))
     # Check if the result was a success
     if protocol.get_payloadcode(result) == protocol.SUCCESS:
         print("success!")
     print(protocol.get_payloaddata(result))
     # Wait for the service to end by joining it's thread
     s.wait()
+
+
